14ce0b0d13fb73a56c0a5b8d7d33af9b073bd445
Amandine Donat-Filliod <amandine.donat.filliod@gmail.com>
2020-03-16 12:26:22 +0100

    02 - Le fichier app.js

--- app.js
+++ app.js
@@ -1,38 +1,9 @@
 const express = require('express');
-const dotenv = require('dotenv');
-const bodyParser = require('body-parser');
-const axios = require('axios');
 const app = express();
-
-dotenv.config();
-
-app.set('view engine', 'pug');
-app.use(bodyParser.urlencoded({ extended: true }));
+const port = 3000;
 
 app.get('/', function(req, res) {
-  res.render('index', { title: 'Qui prend quoi ?' });
+  res.send('Hello people !');
 });
 
-app.post('/party', (req, res) => {
-  axios
-    .post(`${process.env.API_URL}/party`, req.body)
-    .then(({ data }) => res.redirect(`/party/${data._id}`))
-    .catch((err) => res.send(err));
-});
-
-app.get('/party/:id', (req, res) => {
-  axios
-    .get(`${process.env.API_URL}/party/${req.params.id}`)
-    .then(({ data }) =>
-      res.render('party', {
-        party: data,
-        title: data.name,
-        url: `${process.env.FRONT_URL}:${process.env.PORT}/party/${data._id}`,
-      }),
-    )
-    .catch((err) => console.log(err));
-});
-
-app.listen(process.env.PORT, () =>
-  console.log(`Front app listening on port ${process.env.PORT}!`),
-);
+app.listen(port, () => console.log(`Front app listening on port ${port}!`));
