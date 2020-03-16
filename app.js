15cf777f73cc370c06b07b1baebc03fcc6246350
Amandine Donat-Filliod <amandine.donat.filliod@gmail.com>
2020-03-16 12:36:12 +0100

    05 - Variables d'environnement

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const dotenv = require('dotenv').config();
+const port = process.env.PORT;
 
 app.get('/', function(req, res) {
-  res.send('Hello family !');
+  res.send('Hello Taipei !');
 });
 
 app.listen(port, () => console.log(`Front app listening on port ${port}!`));
