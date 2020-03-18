e73235ab3ed3b856bc933be5c4a5a581c1860605
Amandine Donat-Filliod <amandine.donat.filliod@gmail.com>
2020-03-18 12:30:56 +0100

    34 - Page offline

--- sw.js
+++ sw.js
@@ -1,10 +1,17 @@
 addEventListener('install', (event) => {
-  console.log('Hello from the service worker');
+  console.log('Le service worker est installé');
+  event.waitUntil(
+    caches.open('offline').then((cache) => {
+      cache.add('offline.html');
+    })
+  );
 });
 
 addEventListener('fetch', (event) => {
   console.log(event);
   if (event.request.headers.get('Accept').includes('text/html')) {
-    event.respondWith(fetch(event.request));
+    event.respondWith(
+      fetch(event.request).catch(() => caches.match('offline.html')),
+    );
   }
 });
