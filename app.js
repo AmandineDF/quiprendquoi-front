175ea3b15a66a0d90afd2052f622d59f099ffcc6
Amandine Donat-Filliod <amandine.donat.filliod@gmail.com>
2020-03-21 18:44:30 +0100

    App.js comments

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//IMPORTS
 const express = require('express');
 const app = express();
 const dotenv = require('dotenv').config();
@@ -8,7 +9,6 @@ const axios = require('axios');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.use(express.static('pwa'));
-
 app.set('view engine', 'pug');
 
 //INDEX
@@ -16,8 +16,8 @@ app.get('/', function(req, res) {
   res.render('index', { title: 'Qui prend quoi ?' });
 });
 
+//CREATE AN EVENT
 app.post('/party', function(req, res) {
-  //console.log(req.body);
   axios
     .post(`${process.env.API_URL}/party`, req.body)
     .then(({ data }) => res.redirect(`/party/${data._id}`))
@@ -25,18 +25,18 @@ app.post('/party', function(req, res) {
 });
 
 
-//EVENEMENT
+//EVENT CREATED
 app.get('/party/:id', function(req, res) {
   axios
     .get(`${process.env.API_URL}/party/${req.params.id}`)
     .then(({ data }) => {
         console.log(data);
 
-        //Traitement de l'url pour google agenda
+        //Event URL manipulation (for Google Agenda)
         let date = data.date.split('T')[0].replace(/-/gi, '');
         let name = data.name.replace(/ /gi, '+');
         let url = `${process.env.FRONT_URL}:${process.env.PORT}/party/${data._id}`
-        //prblm avec les date en fin de mois
+        //Prblm with end of month (parseInt(date) + 1)
         let googleAgendaLink = `https://calendar.google.com/calendar/r/eventedit?text=${name}&dates=${date}/${parseInt(date) + 1}&ctz=Europe/Paris&trp=true&location=${url}&details=`;
 
         res.render('party', {
@@ -51,6 +51,7 @@ app.get('/party/:id', function(req, res) {
     .catch((err) => console.log(err));
 });
 
+//ADD AN ITEM TO BRING
 app.post('/party/:id/items', function(req, res) {
   console.log(req.body);
   axios
@@ -59,6 +60,7 @@ app.post('/party/:id/items', function(req, res) {
     .catch((err) => res.send(err));
 });
 
+//DESIGNATE WHO BRINGS AN ITEM
 app.post('/party/:id/items/:itemId/user', function(req, res) {
   axios
     .patch(`${process.env.API_URL}/party/${req.params.id}/items/${req.params.itemId}`, req.body)
@@ -66,6 +68,7 @@ app.post('/party/:id/items/:itemId/user', function(req, res) {
     .catch((err) => res.send(err));
 });
 
+//DELETE AN ITEM
 app.post('/party/:id/items/:itemId/delete', function(req, res) {
   axios
     .delete(`${process.env.API_URL}/party/${req.params.id}/items/${req.params.itemId}`, req.body)
@@ -73,4 +76,5 @@ app.post('/party/:id/items/:itemId/delete', function(req, res) {
     .catch((err) => res.send(err));
 });
 
+//CONSOLE MESSAGE
 app.listen(port, () => console.log(`Front app listening on port ${port}!`));
